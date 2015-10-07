cac6c76c340f27a0b98f44125bec69c5b5995f64
LoganArnett <Loganarn5@gmail.com>
2015-10-07 02:00:11 -0400

    added a home page and used the router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,16 @@
 import {View, Component} from 'angular2/angular2';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 import {HomeComponent} from './home.component.ts';
+import {BenchComponent} from './bench.component.ts';
 
 
 @Component({ selector: 'my-app' })
 @View({
-  template: `
-    <header class="jumbotron">
-    <img id="firstHamster" src="https://isowhey.files.wordpress.com/2013/03/shutterstock_92573752.jpg">
-    <h1>Welcome to the Muscle Hamster</h1>
-    <img id="secondHamster" src="https://isowhey.files.wordpress.com/2013/03/shutterstock_92573752.jpg">
-    </header>
-    <router-outlet></router-outlet>
-    `,
+  templateUrl: './app/views/app.html',
   directives: [ROUTER_DIRECTIVES]
 })
 @RouteConfig([
   { path: '/', as: 'Home', component: HomeComponent },
+  { path: '/bench', as: 'Bench', component: BenchComponent }
 ])
 export class AppComponent { }
---
+++ src/app/bench.component.ts
@@ -0,0 +1,57 @@
+import {Component, FORM_DIRECTIVES, View} from 'angular2/angular2';
+
+@Component({ selector: 'my-bench' })
+@View({
+	templateUrl: './app/views/form.html',
+	directives: [FORM_DIRECTIVES]
+})
+export class BenchComponent {
+	public gender = '';
+	public bodyweight = 0;
+	public bench = 0;
+	public count = 0;
+	public performance = "";
+	public img = "";
+
+  strength(){
+   return this.bench / this.bodyweight
+  }
+	measure() {
+		if(this.gender == "Male"){
+      if(this.strength() < 1.25){
+				this.performance = "Poor"
+        this.img = "http://cdn.meme.am/instances/53708159.jpg"
+			}
+      else if(this.strength() >= 2) {
+				this.performance = "ARNOLD"
+        this.img = "http://www.apec-s.com/wp-content/uploads/2013/10/Arnold-pic.jpg"
+			}
+      else if(this.strength() >= 1.75){
+				this.performance = "Excellent"
+        this.img = "http://static.businessinsider.com/image/4eb007c7eab8eab82b00001d/image.jpg"
+			}
+      else {
+				this.performance = "Good"
+        this.img = "http://static.socialitelife.com/uploads/2013/06/19/paul-rudd-underwear-06192013-400x300.jpg"
+			}
+		}
+		else {
+      if(this.strength() < 0.8){
+				this.performance = "Poor"
+        this.img = "http://media.salon.com/2006/12/rosie_vs_donald_the_highlight_reel_so_far.jpg"
+			}
+      else if(this.strength() >= 1.25) {
+				this.performance = "RONDA"
+        this.img = "https://cdn0.vox-cdn.com/thumbor/gIWaQCI43SUnhYozkyUZYV7wkkw=/0x0:1471x1103/400x300/cdn0.vox-cdn.com/uploads/chorus_image/image/46944156/GettyImages-482585372.0.jpg"
+			}
+      else if(this.strength() >= 1){
+				this.performance = "Excellent"
+        this.img = "http://www.bolegaindia.com/images/gossips/serena-williams-post_1368005313.jpg"
+			}
+			else {
+				this.performance = "Good"
+        this.img = "http://www.paperhi.com/thumbnails/detail/20131028/women%20actress%20arrows%20braids%20jennifer%20lawrence%20katniss%20everdeen%20the%20hunger%20games_www.paperhi.com_98.jpg"
+			}
+		}
+	}
+}
